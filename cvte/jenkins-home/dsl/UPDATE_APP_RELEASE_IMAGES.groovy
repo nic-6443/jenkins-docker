fcc326d1cc3c6d859df59cfda0d8ec91af951106
刘创招 <liuchuangzhao@cvte.com>
2019-01-08 17:29:17 +0800

    增加http request插件; 完善鲸云发布流程脚本

--- cvte/jenkins-home/dsl/UPDATE_APP_RELEASE_IMAGES.groovy
+++ cvte/jenkins-home/dsl/UPDATE_APP_RELEASE_IMAGES.groovy
@@ -2,21 +2,37 @@ pipelineJob('UPDATE_APP_RELEASE_IMAGES') {
     definition {
         cps {
             script('''
-                pipeline {
-                    agent any
-                    parameters {
-                        string(name: 'pubType', defaultValue: 'pre-release', description: '发布类型')
-                        string(name: 'pubParam', defaultValue: '{}', description: '发布参数')
-                    }
+pipeline {
+    agent any
+    parameters {
+        string(name: 'pubType', defaultValue: 'pre-release', description: '发布类型')
+        string(name: 'pubParam', defaultValue: '{}', description: '发布参数')
+    }
 
-                    stages {
-                        stage('调用CCloud'){
-                            steps {
-                                ccloudPub "${params.pubType}" "${params.pubParam}"
-                            }
-                        }
-                    }
-                }''')
+    stages {
+        stage('调用CCloud'){
+            steps {
+                ccloudPub "${params.pubType}-${params.pubParam}"
+            }
+        }
+    }
+    
+    post {
+        always {
+            notifyResult("""
+                {
+                    "buildId": "${currentBuild.id}",
+                    "jobName": "${currentBuild.rawBuild.log.bytes.encodeBase64().toString()}"
+                }
+            """)
+        }
+    }
+}
+
+def notifyResult(body) {
+    httpRequest httpMode: 'POST', url: "http://192.0.75.1:8090/callback/jenkins/" , contentType: 'APPLICATION_JSON',requestBody: body
+}
+''')
         }
     }
 }
