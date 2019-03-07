0f4e8057f0034a49540d9c6fc6b9806f90ee07cc
pengpeijie <pengpeijie@cvte.com>
2019-03-07 18:00:57 +0800

    增加：镜像启动时自动执行任务

--- cvte/jenkins-home/init.groovy.d/startup.groovy
+++ cvte/jenkins-home/init.groovy.d/startup.groovy
@@ -12,6 +12,34 @@ buildJob('seed')
 
 Logger.global.info("[Done] startup script")
 
+Logger.global.info("[Running] warm up script")
+
+//Thread.sleep(30000)
+
+jobs = Hudson.instance.getAllItems()
+
+// iterate through the jobs
+for (j in jobs) {
+    def pattern = 'seed';
+    def m = j.getName() =~ pattern;
+    Logger.global.info("job name ==> " + j.getName())
+
+    // if pattern does not match, then run the job
+    if (!m) {
+        // first check, if job is buildable
+        if (j instanceof BuildableItem) {
+            // run that job
+            Logger.global.info("[Running]  warm up task ... ")
+            Logger.global.info(j.getName())
+
+            j.scheduleBuild();
+        }
+    }
+}
+
+// 保证定时任务可以被调度成功
+Thread.sleep(10000)
+
 private void configureSecurity() {
     Jenkins.getInstance().disableSecurity()
 }
